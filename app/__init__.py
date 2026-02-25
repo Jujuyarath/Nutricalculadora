f5b240c9d418da514ecfceb1400facc3d41fac8a
Arath <arath.cg73@outllo.com>
2026-02-25 12:38:42 -0600

    delete archive init

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,8 @@
 from flask import Flask
-import psycopg2
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "1.3.6.4.2.3.45.2.34.523.5423.65_fsd.jyr.nsf.5425.dfg.43.df.sky.ky.gnf.543.dsfghsf."
-
-    # Conexion a la base de datos
-    app.conn = psycopg2.connect(
-        host="dpg-d65t1c0gjchc73fh6i30-a.oregon-postgres.render.com",
-        database="arathlabs_db",
-        user="arathlabs_db_user",
-        password="EVXGekJcJvVGGOnOUiGcOeTiBhUWEWKx",
-        port="5432"
-    )
     
     # Registrar blueprints
     from .routes.panel import panel_bp
@@ -24,5 +14,7 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(calculadora_bp)
     app.register_blueprint(coach_bp)  
+
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
     
     return app
