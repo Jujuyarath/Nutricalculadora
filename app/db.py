ecd71b8e8b61e6c43aa813e3da7ba15170dd430a
Arath <arath.cg73@outllo.com>
2026-02-25 21:37:08 -0600

    ajuste

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ import psycopg
 def get_conn():
     return psycopg.connect(
         host="dpg-d65t1c0gjchc73fh6i30-a.oregon-postgres.render.com",
-        database="arathlabs_db",
+        dbname="arathlabs_db",
         user="arathlabs_db_user",
         password="EVXGekJcJvVGGOnOUiGcOeTiBhUWEWKx",
         port="5432"
