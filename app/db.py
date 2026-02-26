6813606e0b6bfad41085cb813d31a07bf8dc9f1e
Arath <arath.cg73@outllo.com>
2026-02-25 21:22:01 -0600

    psycopg

--- app/db.py
+++ app/db.py
@@ -1,8 +1,8 @@
 # app/db.py
-import psycopg2
+import psycopg
 
 def get_conn():
-    return psycopg2.connect(
+    return psycopg.connect(
         host="dpg-d65t1c0gjchc73fh6i30-a.oregon-postgres.render.com",
         database="arathlabs_db",
         user="arathlabs_db_user",
