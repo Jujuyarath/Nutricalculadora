9d4236337eb0b6d7e50e19591e931aae6a9e2d8b
Jujuyarath <arath.cg73@outlook.com>
2026-02-27 22:59:04 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,9 @@ def create_app():
     database_url = os.getenv("DATABASE_URL")
 
     if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
         
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_url or "postgresql://tu_usuario:tu_pass@localhost/tu_db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url or "postgresql+psycopg://tu_usuario:tu_pass@localhost/tu_db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Inicializar DB y migraciones
